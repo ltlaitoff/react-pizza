6764604f28538b0ebc45593d3de512fe73df9393
Ivan Baluta <ltlaitoff@gmail.com>
2021-12-19 19:49:26 +0200

    fix: changed logo tag from div to a, flex-inline

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 import logo from '../../assets/images/logo.png'
 import styles from "./logo.scss"
 
-const Logo = () => {
-  return (
-		<div className='logo' style={styles}>
+const Logo = ({ href }) => {
+	return (
+		<a href={href} className='logo' style={styles}>
 			<img src={logo} alt='' className='logo-icon' />
 			<div className='logo-text'>
 				<h1 className='logo-title'>REACT PIZZA</h1>
 				<div className='logo-descr'>самая вкусная пицца во вселенной</div>
 			</div>
-		</div>
+		</a>
 	)
 }
 
