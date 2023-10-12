304c587c343ecfe90cc651a6ad433fba0209b349
ltlaitoff <ltlaitoff@gmail.com>
2023-10-12 17:05:20 +0300

    feat: Changed for dephoy to github pages

--- src/api/api.js
+++ src/api/api.js
@@ -1,51 +1,76 @@
-const domen = 'http://localhost:3000'
+import data from './database.json'
+// const domen = 'http://localhost:3000'
 
-const getCategories = () => {
-	return fetch(domen + '/categories').then(response => {
-		return response.json()
-	})
+const getCategories = async () => {
+	return await data.categories
+	// return fetch(domen + '/categories').then(response => {
+	// 	return response.json()
+	// })
 }
 
-const getPizzas = (
+const getPizzas = async (
 	filterName = '',
 	filterValue = '',
 	sorting = '',
 	sortingOrder = 'asc'
 ) => {
-	const url = new URL(domen + '/pizzas')
+	return await data.pizzas
+		.filter(item => {
+			if (filterName !== '' && filterValue !== '-1') {
+				return item[filterName].includes(filterValue)
+			}
 
-	if (filterName !== '' && filterValue !== '-1') {
-		url.searchParams.append(filterName + '_like', filterValue)
-	}
+			return true
+		})
+		.sort((a, b) => {
+			const aElement = a[sorting]
+			const bElement = b[sorting]
 
-	url.searchParams.append('_sort', sorting)
-	url.searchParams.append('_order', sortingOrder)
+			if (sortingOrder === 'asc') return aElement - bElement
 
-	return fetch(url.toString()).then(response => {
-		return response.json()
-	})
+			return bElement - aElement
+		})
+	// const url = new URL(domen + '/pizzas')
+
+	// if (filterName !== '' && filterValue !== '-1') {
+	// 	url.searchParams.append(filterName + '_like', filterValue)
+	// }
+
+	// url.searchParams.append('_sort', sorting)
+	// url.searchParams.append('_order', sortingOrder)
+
+	// return fetch(url.toString()).then(response => {
+	// 	return response.json()
+	// })
 }
 
-const getPizzasByIds = ids => {
-	const url = new URL(domen + '/pizzas')
-
-	ids.map(id => url.searchParams.append('id', id))
-
-	return fetch(url.toString()).then(response => {
-		return response.json()
+const getPizzasByIds = async ids => {
+	return await data.pizzas.filter(item => {
+		return ids.includes(item.id)
 	})
+	// const url = new URL(domen + '/pizzas')
+
+	// ids.map(id => url.searchParams.append('id', id))
+
+	// return fetch(url.toString()).then(response => {
+	// 	return response.json()
+	// })
 }
 
-const getSizes = () => {
-	return fetch(domen + '/sizes').then(response => {
-		return response.json()
-	})
+const getSizes = async () => {
+	return await data.sizes
+
+	// return fetch(domen + '/sizes').then(response => {
+	// 	return response.json()
+	// })
 }
 
-const getDoughs = () => {
-	return fetch(domen + '/dough').then(response => {
-		return response.json()
-	})
+const getDoughs = async () => {
+	return await data.dough
+
+	// return fetch(domen + '/dough').then(response => {
+	// 	return response.json()
+	// })
 }
 
 export { getCategories, getPizzas, getSizes, getDoughs, getPizzasByIds }
